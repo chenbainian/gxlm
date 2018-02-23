3a4eccb70a0177ac1843e734d4362afe625e6a86
chenbonian <bonian.chen@e-dewin.com>
2018-02-23 18:06:08 +0800

    init

--- public/static/admin/js/public.js
+++ public/static/admin/js/public.js
@@ -27,14 +27,14 @@ var pub = {
         }, true);
     },
     accMul: function (arg1,arg2){
-    var m=0,s1=arg1.toString(),
-        s2=arg2.toString();
-    try{
-        m+=s1.split(".")[1].length}catch(e){}
-    try{
-        m+=s2.split(".")[1].length}catch(e){}
-    return Number(s1.replace(".",""))*Number(s2.replace(".",""))/Math.pow(10,m
-        )},
+        var m=0,s1=arg1.toString(),
+            s2=arg2.toString();
+        try{
+            m+=s1.split(".")[1].length}catch(e){}
+        try{
+            m+=s2.split(".")[1].length}catch(e){}
+        return Number(s1.replace(".",""))*Number(s2.replace(".",""))/Math.pow(10,m)
+    },
 //根据id删除
     del_by_id: function (obj, success, fail) {
         var url = obj.data('url');
